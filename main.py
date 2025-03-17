3f5a3bb418ee5176ff7688ed7f0a98b0eb907d73
Jschammy <schamel.an.justin@gmail.com>
2025-03-17 11:06:49 -0400

    Updated main.py with 100 Days of Code project 15 (CoffeeMachine) starting code, added resource checking functionality, resource report functionality, and machine off functionality.

--- main.py
+++ main.py
@@ -1,3 +1,55 @@
-message = input("What is this project about?")
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-print(message)
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+def check_resources(drink_ingredients):
+    for item in drink_ingredients:
+        if drink_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+machine_is_on = True
+
+while machine_is_on:
+    drink_choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if drink_choice == "off":
+        machine_is_on = False
+    elif drink_choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[drink_choice]
+        check_resources(drink["ingredients"])
